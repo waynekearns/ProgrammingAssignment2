8bc7b8e9420b6133662a504c456148e863d4fd8f
waynekearns <wlkearns@gmail.com>
2014-10-26 17:53:46 -0500

    Assignment sort of complete
    I have read that there are faster ways to do this based on the data in
    the matrix, but this is my first best guess.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The intent of these functions are to cache a matrix and it's inverse at invocation such that
+## the inverse will be cached for re-use later. This is especially useful if the inverse of a 
+## matrix is very likely to be used.
 
-## Write a short comment describing this function
+## The below function sets the matrix and uses the solve function to inverse the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  sImatrix<-function(solve) m<<- solve
+  gImatrix<-function() m
+  list(set=set, get=get,
+       sImatrix = sImatrix,
+       gImatrix = gImatrix)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function computes the inverse of the matrix. If the inverse has already been calculated
+## and the matrix is unchanged, it should be retrieved.
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$gImatrix()
+  if(!is.null(m)){
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$sImatrix(m)
+  m
 }
+
+
+       
+
+
